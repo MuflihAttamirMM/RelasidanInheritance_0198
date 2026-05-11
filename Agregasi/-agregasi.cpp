bc1cecde733f928373a81a649443f294cb1244d5
Muflay <muflihattamirmuslim@gmail.com>
2026-05-11 15:30:48 +0700

    Perbaikan Error di Agregasi

--- Agregasi/-agregasi.cpp
+++ Agregasi/-agregasi.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-#include "anak.h"
-#include "ibu.h"
+#include "-anak.h"
+#include "-ibu.h"
 
 int main() { //agregasi
     ibu* varIbu = new ibu("dini");
